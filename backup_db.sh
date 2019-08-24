7dddba2df7f049adcbc254ad8a5f63231d50d201
w2jd <nullvszero@gmail.com>
2019-08-24 21:16:24 +0900

    modify format

--- backup_db.sh
+++ backup_db.sh
@@ -16,16 +16,16 @@ DAYS_KEEP=30
 DB_USER=[Backup user id]
 DB_PASS=[Backup user password]
 
-# show Databases
+### show Databases
 /usr/local/mariadb10/bin/mysqlshow --user=$DB_USER --password=$DB_PASS
 
-# remove backups older than $DAYS_KEEP
+### remove backups older than $DAYS_KEEP
 find ${DIR}* -mtime +$DAYS_KEEP -exec rm -f {} \; > /dev/null
 
-# Backup full database
+### Backup full database
 FULLDB=${DIR}FULL-${DATESTAMP}.sql
 /usr/local/mariadb10/bin/mysqldump --user=$DB_USER --password=$DB_PASS --opt --all-databases --flush-logs > ${FULLDB}
 
-# Backup database - individual
+### Backup database - individual
 DBNAME=[DB Name]
 /usr/local/mariadb10/bin/mysqldump --user=$DB_USER --password=$DB_PASS --opt ${DBNAME} > ${DIR}${DBNAME}-${DATESTAMP}.sql
