9d3aeec16c6edb2073272b0f1117186befaa3c32
w2jd <zprobdorqz@gmail.com>
2019-08-27 23:30:08 +0900

    Edited to make it easier to see.

--- geoip.bat
+++ geoip.bat
@@ -11,5 +11,7 @@ REM OS          : Windows
 IF "%1"=="-h" (
 	echo %0 [IP]
 ) ELSE (
-	echo %1 | [Wireshark Path]\WiresharkPortable\App\Wireshark\mmdbresolve.exe -f [Wireshark Path]\WiresharkPortable\GeoLite2-Country\GeoLite2-City.mmdb
+	echo %1 | [Wireshark Path]\WiresharkPortable\App\Wireshark\mmdbresolve.exe -f [Wireshark Path]\WiresharkPortable\GeoLite2-Country\GeoLite2-ASN.mmdb | findstr "autonomous_system_organization autonomous_system_number"
+	echo.
+	echo %1 | [Wireshark Path]\WiresharkPortable\App\Wireshark\mmdbresolve.exe -f [Wireshark Path]\WiresharkPortable\GeoLite2-Country\GeoLite2-City.mmdb | findstr "country.iso_code country.names.en location.latitude location.longitude location.accuracy_radius"
 )
